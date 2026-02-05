d1be720700efdf2a8df886148e14e71869982280
wilsxnjr <99342358+wilsxnjr@users.noreply.github.com>
2026-02-05 22:18:12 +0200

    Introduction of generics

---
+++ generics-intro/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+
+    }
+}
