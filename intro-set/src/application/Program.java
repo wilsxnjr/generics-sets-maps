b125886cf64e97e2e209fa7fd43617f86c4c9780
wilsxnjr <99342358+wilsxnjr@users.noreply.github.com>
2026-02-10 13:28:03 +0200

    Introduction set

---
+++ intro-set/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("Tv");
+        set.add("Tablet");
+        set.add("Notebook");
+
+        //set.remove("Tv");
+        set.removeIf(x -> x.charAt(0) == 'T');
+
+
+        for (String p : set) {
+            System.out.println(p);
+        }
+
+    }
+}
